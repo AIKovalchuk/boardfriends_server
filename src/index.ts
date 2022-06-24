a086c8434c9883e3253344685d04c7b5135a7a26
Alexandr Kovalchuk <aikovalchuk@ya.ru>
2022-06-25 00:50:55 +0300

    Hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log(`Server has been started on port ${port}`);
+});
